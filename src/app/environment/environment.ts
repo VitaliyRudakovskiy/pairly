3fe539d8b0de315e6246371b0a93b5768b6ec1a7
Vitaliy Rudakovskiy <vitalirud03@gmail.com>
2025-12-12 16:13:06 +0300

    feat: configure firebase

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAZ0Skfl98Dd8JEa7bYCNMky_SZ8NWLVFM',
+    authDomain: 'pairly-f3add.firebaseapp.com',
+    projectId: 'pairly-f3add',
+    storageBucket: 'pairly-f3add.firebasestorage.app',
+    messagingSenderId: '759118536172',
+    appId: '1:759118536172:web:df44df9d21e52187b7f4c9',
+  },
+};
